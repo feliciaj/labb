09fff1994185b08135779090a6bc2506a20c176b
Felicia Joneby <felicia@joneby.com>
2015-02-09 15:03:16 +0100

    hejdå

--- ByggserverLab/Program.cs
+++ ByggserverLab/Program.cs
@@ -10,7 +10,7 @@ namespace ByggserverLab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! hejdå");
         }
     }
 }
